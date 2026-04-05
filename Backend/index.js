e0255d2c635ed141abdeb52e980bc9ae2156fa00
Farheen <farheen.ahmed2004@gmail.com>
2026-04-05 18:04:31 +0530

    chore: initialize package.json with backend dependencies and scripts

--- Backend/index.js
+++ Backend/index.js
@@ -7,14 +7,18 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 const app = express();
-
+const dirname = path.resolve();
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
+
 
 const corsOptions = {
   origin: ["http://localhost:5173"],
@@ -32,7 +36,10 @@ app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
-
+app.use(express.static(path.join(dirname, "/Frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(dirname, "Frontend", "dist", "index.html"));
+});
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
